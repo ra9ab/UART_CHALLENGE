e7f5107e56daa379c4a60c4a4fce2c2a3c1ae4d4
Mohamed Ragab <50461567+ra9ab@users.noreply.github.com>
2019-10-13 15:07:27 +0200

    adding keypad driver

---
+++ keypad.h
@@ -0,0 +1,41 @@
+ /******************************************************************************
+ *
+ * Module: KEYPAD
+ *
+ * File Name: keypad.h
+ *
+ * Description: Header file for the Keypad driver
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+#include "macros.h"
+#include "registers.h"
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+
+/* Keypad configurations for number of rows and columns */
+#define N_col 3
+#define N_row 3
+
+/* Keypad Port Configurations */
+#define KEYPAD_PORT_OUT REG_PORTC
+#define KEYPAD_PORT_IN  REG_PINC
+#define KEYPAD_PORT_DIR REG_DDRC
+#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+
+/*
+ * Function responsible for getting the pressed keypad key
+ */
+uint8 KeyPad_getPressedKey(void);
+
+#endif /* KEYPAD_H_ */
